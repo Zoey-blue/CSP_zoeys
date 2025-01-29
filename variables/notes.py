3d1dc12947339976c14c7100430bdee64ac5cdab
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-01-29 11:42:50 -0700

    python notes

--- variables/notes.py
+++ variables/notes.py
@@ -1,3 +1,12 @@
-# zoey sosa, Variable
+# zoey sosa, Variable notes
 
-print("hello")
+
+subject = input("what is your favorite subject: ")
+
+small_number = 4
+
+name = input("What is your name: ")
+
+print(name, "really likes", subject)
+
+name = "alex"
