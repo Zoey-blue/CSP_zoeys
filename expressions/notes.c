a4fa73a2b691ca9c78231baa5a276cad462b68ab
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-04 11:17:30 -0700

    expression notes python

--- expressions/notes.c
+++ expressions/notes.c
@@ -1,11 +1,17 @@
 //zoey sosa, Expressions Notes C
 #include <stdio.h>
 #include <math.h>
-float x = pow (2,2);
-float equation = 5*7+11(5-2);
+//float x = pow (2,2);
+//float equation = 5*7+11(5-2);
+char name[] = "zoey";
+int num = 12;
+float pi = 3.1415926535;
 // + - * /
 int main(void){
-
+    num = 4;
+    printf("%d\n",num);
    // printf("%f\n",equation);
+
+   printf("%.20f",pi);
     return 0;
 }
--- expressions/notes.py
+++ expressions/notes.py
@@ -1,2 +1,21 @@
 # Zoey Sosa, Expressions Notes Pythons
 
+# zoey sosa, python program 
+
+#print(70+8)
+#print(70*8)
+#print(70**8)
+#print(70//8) #integer division returns just teh whole number 
+#print(70%8) #modulo/Modulus/MOD return the remainer
+
+num = 4
+
+print(num)
+
+num += 1
+
+print(num)
+
+pi = 3.1415
+
+print(f"{pi:.4f}")
