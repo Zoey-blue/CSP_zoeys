33c0563d906e131c6775a3ed4d18067687506e33
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-04 11:49:03 -0700

    expression pratice python

--- expressions/notes.py
+++ expressions/notes.py
@@ -5,7 +5,7 @@
 #print(70+8)
 #print(70*8)
 #print(70**8)
-#print(70//8) #integer division returns just teh whole number 
+#print(70//8) #integer division returns just the whole number 
 #print(70%8) #modulo/Modulus/MOD return the remainer
 
 num = 4
--- expressions/practice.py
+++ expressions/practice.py
@@ -1,3 +1,12 @@
 # Zoey Sosa, Expression pratice python 
 
-print()
+print(7-24/8*4+6)
+
+print(18/3-7+2*5)
+
+print(6*4/12+72/8-9)
+
+print((17-6/2)+4*3)
+
+print(-2(1*4-2/2)+(6+2-3))
+
