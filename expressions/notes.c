65a84bf8371def8bf79165490975656d6a9615e0
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-05 11:20:38 -0700

    fixing files

--- expressions/notes.c
+++ expressions/notes.c
@@ -6,12 +6,13 @@
 char name[] = "zoey";
 int num = 12;
 float pi = 3.1415926535;
+float expression = 5*(int)pow(7,2)/4;
 // + - * /
 int main(void){
     num = 4;
-    printf("%d\n",num);
-   // printf("%f\n",equation);
-
-   printf("%.20f",pi);
+    //printf("%d\n",num);
+    //printf("%f\n",equation);
+    //printf("%f", expression);
+    //printf("%.20f",pi);
     return 0;
 }
