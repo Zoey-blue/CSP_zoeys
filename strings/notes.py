a5a63e3b789ccfa1d1a0704479a777d7113937da
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-10 11:49:04 -0700

    string notes python

---
+++ strings/notes.py
@@ -0,0 +1,15 @@
+# Zoey Sosa, Strings notes python
+
+# strings is a data type that holds any info surround by quotation marks "" ' '
+
+note = "vienna's class"
+
+name = input("what is ypur first name\n").strip().capitalize()
+
+#print(f"hello {name} welcome to my program!")
+print("this is your name"+ name)
+
+print(note)
+
+sentence= "the quick brown fox jumps over the lazy dog"
+
