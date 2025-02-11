d8f603e48f09b3e202716f78b656ab823eba60be
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-11 16:30:28 -0700

    finance calc C

--- expressions/finance_calculator.c
+++ expressions/finance_calculator.c
@@ -1,10 +1,11 @@
 //zoey sosa, TEMPLATE
 #include <stdio.h>
 
-float income,rent,utilities, groceries, transprtation;
+float income,rent,utilities, groceries, transprtation,savings,spendings;
 
 int main(void){
 printf("what is your income?");
+scanf("%s",income);
 
     return 0;
 }
