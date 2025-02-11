b3637a5abdfb081a6fe5fb40ff9627ed443dd610
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-11 11:16:24 -0700

    notes for string python

--- strings/notes.py
+++ strings/notes.py
@@ -2,14 +2,19 @@
 
 # strings is a data type that holds any info surround by quotation marks "" ' '
 
-note = "vienna's class"
+#note = "vienna's class"
 
-name = input("what is ypur first name\n").strip().capitalize()
+#name = input("what is ypur first name\n").strip().capitalize()
 
 #print(f"hello {name} welcome to my program!")
-print("this is your name"+ name)
+#print("this is your name "+  name)
 
-print(note)
+#print(note)
 
-sentence= "the quick brown fox jumps over the lazy dog"
+sentence = "the quick brown fox jumps over the lazy dog"
 
+#print(len(sentence))
+#print(sentence[4])
+start = sentence.find("brown")
+length = len("brown fox")
+print(sentence[start:start+length])
