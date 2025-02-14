606c7ac6f0bc51813c6a11adedbd5a8e12cd1053
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-14 11:35:23 -0700

    name decorator

--- strings/name_decorator.c
+++ strings/name_decorator.c
@@ -1,15 +1,14 @@
 //zoey sosa, name decorator C
 #include <stdio.h>
 
-
-char name [20];
+    char name [2000];
+   
 int main(void){
-    printf("hello welcome to your personal name decorator");
-    scanf ("%s, &name");
-
-    char decor1[];
-    char decor2[];
-    decor1= "^^^";
-    decor2= "^^^";
+    printf("hello welcome to your personal name decorator\n");
+    printf("What is your name?\n");
+    scanf ("%s", name);
+    char decor1 [20] ="^^^";
+    char decor2 [20] = "^^^";
+    printf("%s%s%s", decor1, name, decor2);
     return 0;
 }
