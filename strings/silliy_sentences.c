9ed540b8e161f0be2280bf59df3670794eca2cfa
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-18 16:32:30 -0700

    silly sentences

--- strings/silliy_sentences.c
+++ strings/silliy_sentences.c
@@ -1,15 +1,18 @@
 //zoey sosa, silly sentences C
 #include <stdio.h>// empty string variables for user words (minimum 3)
 
+char celebrity[20];
+char place[20];
+char food[20];
 
 int main(void){
     // A welcome for the user telling them what the program is (print)
     printf("welcome to this silly sentences maker! awser the folling questions");
     //ask user for words (print statement with a quesion scanf to set to a variable) 
     //(in C we need to tell the user that they can only type 1 word)
-    printf()
+    print 
     // print out the story with the varables inserted (" welcome %s to my program", name)
-
+    //printf("You were walking when", celebrity ,"saw you and you guys walked to the", place, "and ate",food,".");
     
     return 0;
 }
