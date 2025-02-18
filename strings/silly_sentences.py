f17cb1ae101b0ea976e0f4c5b078caaf8f1a8214
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-18 16:13:20 -0700

    silly sentences

--- strings/silly_sentences.py
+++ strings/silly_sentences.py
@@ -1,2 +1,11 @@
 # Zoey sosa silly sentences pthyon
 
+print("Hello welcome to your silly sentence maker! Answer the following questions!")
+
+celebity = input("what is your favortive celebity?\n")
+
+place = input ("what is your favortive place?\n")
+
+food = input ("what is a food you enjoy?\n")
+
+print("you were walking when", celebity ,"saw you and you guys walked to the", place, "and ate",food,".")
