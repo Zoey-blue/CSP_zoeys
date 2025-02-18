1e13c5203fa0dbef429f5593a6d5ab4a9e595aa7
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-18 16:20:51 -0700

    silly sentences

--- strings/silliy_sentences.c
+++ strings/silliy_sentences.c
@@ -4,11 +4,12 @@
 
 int main(void){
     // A welcome for the user telling them what the program is (print)
-
+    printf("welcome to this silly sentences maker! awser the folling questions");
     //ask user for words (print statement with a quesion scanf to set to a variable) 
     //(in C we need to tell the user that they can only type 1 word)
-
+    printf()
     // print out the story with the varables inserted (" welcome %s to my program", name)
+
     
     return 0;
 }
--- strings/silly_sentences.py
+++ strings/silly_sentences.py
@@ -2,10 +2,10 @@
 
 print("Hello welcome to your silly sentence maker! Answer the following questions!")
 
-celebity = input("what is your favortive celebity?\n")
+celebrity = input("what is your favorite celebrity?\n")
 
-place = input ("what is your favortive place?\n")
+place = input ("what is your favorite place?\n")
 
 food = input ("what is a food you enjoy?\n")
 
-print("you were walking when", celebity ,"saw you and you guys walked to the", place, "and ate",food,".")
+print("You were walking when", celebrity ,"saw you and you guys walked to the", place, "and ate",food,".")
