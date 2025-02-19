b6ca60a83063a2b90e65b1c73a536e470e02942c
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-19 11:39:07 -0700

    silly sentences

--- functions/notes.c
+++ functions/notes.c
@@ -3,8 +3,12 @@
 int num;
 char name [50],verb[50];
 void add(int numOne, int numTwo){
-    return numOne+numTwo;
+    //return numOne+numTwo;
 }
+void due(char assignment[50], char day[20]){
+    printf("The %s assignment is due %s", assignment, day);
+}
+
 const char* word(type){
    char choice[50];
    printf("please give me %s:\n", type);
@@ -12,9 +16,13 @@ const char* word(type){
 }
 
 int main(void){
-    printf("please tell me a number");
-    scanf("%d\n, num");
-    add(4,10);
-    add(7,10);
+    //printf("please tell me a number");
+    //scanf("%d\n, num");
+    //add(4,10);
+    //add(7,10);
+    due("functions notes", "today");
+    due("hello world update", "tomorrow");
+    due("hello world update", "today");
     return 0;
+
 }
--- strings/silliy_sentences.c
+++ strings/silliy_sentences.c
@@ -1,18 +1,25 @@
 //zoey sosa, silly sentences C
 #include <stdio.h>// empty string variables for user words (minimum 3)
 
-char celebrity[20];
-char place[20];
-char food[20];
+char celebrity[50];
+char place[50];
+char food[50];
 
 int main(void){
     // A welcome for the user telling them what the program is (print)
-    printf("welcome to this silly sentences maker! awser the folling questions");
+    printf("Welcome a silly sentences maker! Answer the following questions! (please only one word answers!!)\n");
     //ask user for words (print statement with a quesion scanf to set to a variable) 
-    //(in C we need to tell the user that they can only type 1 word)
-    print 
-    // print out the story with the varables inserted (" welcome %s to my program", name)
-    //printf("You were walking when", celebrity ,"saw you and you guys walked to the", place, "and ate",food,".");
+
+    printf("What is you favorite celebrity?\n");
+    scanf("%s", celebrity);
+    printf("What is your favorite place to be?\n");
+    scanf("%s", place);
+    printf("What is you favorite food?\n");
+    scanf("%s", food);
     
+    //(in C we need to tell the user that they can only type 1 word)
+
+    // print out the story with the varables inserted (" welcome %s to my program", name)
+    printf("You were walking when %s saw you and you guys walked to the %s and ate %s", celebrity, place, food);
     return 0;
 }
