a02614512efff2008e9040825197e6d2c423ed29
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-24 16:31:23 -0700

    finance calculator update python

--- functions/fincansial_cal_update.py
+++ functions/fincansial_cal_update.py
@@ -3,24 +3,16 @@ def info(cost, income, type):
     percentage= cost/income *100
     print(f"your {type} is ${cost:.2f} rent which is,{percentage}%, of your income")
 
-def info():
-    print(f"what is your {}?\n")
-# print statment that welcomes user and tells what program dose
+def user(type):
+    quesition = "what is your monthly " + type + "?\n"
+    return float(input(quesition))
+income = user("income")
+rent = user("rent")
+utilities= user("utilities")
+groceries= user("groceries")
+transportation= user("transportation")
 
-# ask what their income is (varible an input)
-income= float(input("what is your income?\n"))
-# ask what their rent is (varible an input)
-rent= float(input("what is your rent?\n"))
-# ask what their utlitites is (varible an input)
-utilities= float(input("what is your utilities?\n"))
-# ask what their groceries is (varible an input)
-groceries= float(input("what is your groceries?\n"))
-# ask what their transportation is (varible an input)
-transportation= float(input("what is your transportation?\n"))
-# calculate savings as 10% of income (income*.1) (variable)
-saving= income*.1
-# calculate spending as income-saving-rent-utilites-groceries-transportation (variable)
-spending= income-saving-rent-utilities-groceries-transportation
+spending= user ("spending")
 
 info(rent, income, "rent")
 info(utilities, income, "utilites")
