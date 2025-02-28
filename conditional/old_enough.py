ac56938743d68c9dc653907a1e08e078551f30c1
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-02-28 16:07:47 -0700

    old enough

--- conditional/old_enough.py
+++ conditional/old_enough.py
@@ -1,28 +1,15 @@
 # Zoey Sosa Old Enough python
 
-age = input("How old are you?:\n")
+age = int(input("How old are you?:\n"))
 
 
-if age ==17:
-        print("You can vote!")
-elif not age >17:
-    print("you can't vote")
-
-
-if age == 16:
+if age >=18:
+   print("You can vote!")
+elif age <18 and age >= 16:
     print("You can drive!")
-elif not age >16:
-    print("you can't drive") 
-
-
-if age == 15:
+elif age <16 and age >= 15:
     print("You can get a learners permit")
-elif not age >15:
-    print("you can't get a learners permit")
-
-
-if age == 4:
+elif age >= 4:
     print("You can go to school!")
-elif not age >4:
-    print("you can't go to school")
-
+else:
+     print("too bad you aren't any of these ages")
