69eef1cf194fb9870d4cfae7b0e11a0d50dde3e0
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-03-03 04:55:54 +0000

    hello world c update

--- functions/hello_world.c
+++ functions/hello_world.c
@@ -1,10 +1,13 @@
 //zoey sosa, hello world update python
 #include <stdio.h>
+#include <math.h>
 
 void add(char name[50]){
       printf("Hello %s!\n",name);
 }
 int main(void){
+    char name[50];
+    
     add("Jasper");
     add("Zoey");
     add("Josephine");
