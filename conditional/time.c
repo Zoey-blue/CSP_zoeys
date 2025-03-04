e48a75b4182b71627ee3736832ae9c5423860b1e
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-03-04 22:34:22 +0000

    time of day c

--- conditional/time.c
+++ conditional/time.c
@@ -1,30 +1,20 @@
-//zoey sosa, getting the time
+//zoey sosa, time of day
 #include <stdio.h>
 #include <time.h>
 
 
 int main(void){
-    //time since jan 1 1970
-    time_t seconds;
-
-    seconds = time(NULL);
-    //printf("seconds since jan 1, 1970 = %d\n", seconds);
-
-    //current time
-    time_t rawtime;
-    struct tm * timeinfo;
-
-    time(&rawtime);
-    timeinfo= localtime(&rawtime);
-    //printf("current time and date is %s\n", asctime(timeinfo));
-
-    //current hour 
-    time_t now = time(NULL);
-
-    struct tm * tm_struct = localtime(&now);
-    int hour = tm_struct->tm_hour;
-    printf("%d\n", hour); // military time! (0-23)
-
-
-    return 0;
+    time_t now= time(NULL);
+    struct tm*tm_struct = localtime(&now);
+    int hour= tm_struct->tm_hour;
+    printf ("%d\n", hour);
+    
+if (hour<=12){
+    printf ("good morning");
+}else if (hour<= 17){
+    printf("good afternoon");
+}else {
+    printf("good evening");
+}
+return 0;
 }
--- conditional/time.py
+++ conditional/time.py
@@ -1,22 +1,33 @@
 # Zoey Sosa, how to get the Time of Day python
-import time
-
+    #import time
 #first instance of time in programming 
-first_time = time.gmtime()
+    #first_time = time.gmtime()
 #print(first_time)
 
 #current tiem in seconds 
-current = time.time()
+    #current = time.time()
 #print(current) #seconds since Jan 1 1970
 
 #Current date and time like we see it normally 
-now=time.ctime(current)
+    #now=time.ctime(current)
 #print(now)
 
 #get just a part of the time
-local_time = time.localtime(current)
-day=local_time.tm_wday
-hour = local_time.tm_hour #military time (0-23)
-print(hour)
+    #local_time = time.localtime(current)
+    #day=local_time.tm_wday
+    #hour = local_time.tm_hour #military time (0-23)
+#print(hour)
 
+import time
 
+current = time.time()
+
+local_time = time.localtime
+hour = local_time.tm_hour
+
+if hour <=12:
+    print("good moning")
+elif hour <=17:
+    print("good afternoon")
+else:
+    print("good evening")
