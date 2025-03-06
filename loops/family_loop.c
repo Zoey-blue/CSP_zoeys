2c620c86eadfee8dccfb969e5234236031420b4e
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-03-06 11:37:32 -0700

    family loops

--- loops/family_loop.c
+++ loops/family_loop.c
@@ -3,8 +3,8 @@
 
 int main(void){
     char sibilings[][20] = {"Sabrina","Maisha", "Cinthia"};
-printf("Hello %s!\n");
-printf("Hello %s!\n");
-printf("Hello %s!\n");
+printf("Hello %s!\n", sibilings[0]);
+printf("Hello %s!\n" , sibilings[1]);
+printf("Hello %s!\n", sibilings[2]);
     return 0;
 }
