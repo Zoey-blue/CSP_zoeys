c1eb131f2a22dc4deb105c183a8ff53e84d9adbd
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-03-10 06:49:10 +0000

    fizzbuzz

--- loops/fizzBuzz.py
+++ loops/fizzBuzz.py
@@ -1,15 +1,11 @@
 # Zoey Sosa, FizzBuzz python
 
-import range
-x = 3 #varible to keep count of iteration
-num = range(1,50)
-
-while x <=50:
-    if x == :
-    print("fizz")
-    break# tells the loop to stop 
-else :
-    print("")
-    x+=1
-
-    
+for x in range(0,51):
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("buzz")
+    else:
+        print(x)
--- loops/fizzbuzz.c
+++ loops/fizzbuzz.c
@@ -1,8 +1,17 @@
 //zoey sosa, FizzBuzz C
 #include <stdio.h>
 
-int main(void){
-    
-    printf("");
+int main(){
+    for (int x = 1; x <= 50; x++){
+        if (x % 3 == 0 && x % 5 == 0) {
+            printf("fizzbuzz\n");
+        } else if (x % 3 == 0) {
+            printf("fizz\n");
+        } else if (x % 5 == 0) {
+            printf("buzz\n");
+        } else { 
+            printf("%d\n", x);
+        }
+    }
     return 0;
 }
