53a33aa83c030ece522debcdb0e4f26c02e9c50a
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-04-28 11:05:13 -0600

    hiddenimage

--- javascript/hw.js
+++ javascript/hw.js
@@ -1,4 +1,4 @@
-let images = ["https://cdn.vectorstock.com/i/1000v/12/03/background-different-colors-separated-squares-vector-32141203.jpg , https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYsCeROwcvwl23u8kHo6vMxV4y5cn3hZ-VqUcN7tZsX1eLnoD4MGb6a9rMC1au1JeidKI& "]
+let images = ["https://cdn.vectorstock.com/i/1000v/12/03/background-different-colors-separated-squares-vector-32141203.jpg" , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYsCeROwcvwl23u8kHo6vMxV4y5cn3hZ-VqUcN7tZsX1eLnoD4MGb6a9rMC1au1JeidKI& "]
 let counter = 0 
 
 function change (){
@@ -14,4 +14,7 @@ function change (){
     }
     document.getElementById("img")
     src=images[counter]
+}
+function change (){
+    document.getElementById("image").src -= "https://cdn.vectorstock.com/i/1000v/12/03/background-different-colors-separated-squares-vector-32141203.jpg "
 }
