8d4047b8f4459f81e2e84a72c85e49012338a944
Zoey-blue <zoey.sosa@ucas-edu.net>
2025-04-30 05:05:06 +0000

    css

--- javascript/hidden.js
+++ javascript/hidden.js
@@ -1,20 +1,8 @@
-let images = ["https://cdn.vectorstock.com/i/1000v/12/03/background-different-colors-separated-squares-vector-32141203.jpg" , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYsCeROwcvwl23u8kHo6vMxV4y5cn3hZ-VqUcN7tZsX1eLnoD4MGb6a9rMC1au1JeidKI& "]
-let counter = 0 
-
-function change (){
-    if (counter < images.length){
-        ("img").src = images
-        [counter]
-        counter += 1 
-    }else{
-        counter = 0
-        document.getElementById
-        ("img").src = images
-        [counter]
+function view(){
+    if(document.getElementById("img2").style.display ==="block"){
+            document.getElementById("img2").style.display = "none"
     }
-    document.getElementById("img")
-    src=images[counter]
-}
-function change (){
-    document.getElementById("image").src -= "https://cdn.vectorstock.com/i/1000v/12/03/background-different-colors-separated-squares-vector-32141203.jpg "
+    else{
+        document.getElementById("img2").style.display = "block"
+    }
 }
